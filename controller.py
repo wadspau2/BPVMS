a0eb216505aa8c274a330977c4e1ce10223e6eb2
wadspau2 <wadspau2@gmail.com>
2020-11-28 20:35:04 -0700

    f

--- controller.py
+++ controller.py
@@ -125,6 +125,11 @@ class screen:
         menu1_draw.text((self.line_start,self.line_list[1]),self.menu1_options[1],font=self.fnt,fill=self.color_white if self.menu1_line_index != 1 else self.color_black)
         menu1_draw.text((self.line_start,self.line_list[2]),self.menu1_options[2],font=self.fnt,fill=self.color_white if self.menu1_line_index != 2 else self.color_black)
         menu1_draw.text((self.line_start,self.line_list[3]),self.menu1_options[3],font=self.fnt,fill=self.color_white if self.menu1_line_index != 3 else self.color_black)
+        menu1_draw.polygon([(0,self.line_list[5]),
+                            (self.width,self.line_list[5]),
+                            (self.width,self.line_list[6]),
+                            (0,self.line_list[6])],
+                            fill=self.color_black)
         menu1_draw.text((self.line_start,self.line_list[5]),"P: %.2f hPa" % self.controller.user_interface.LPS35HW.pressure,font=self.fnt,fill=self.color_white)
         self.controller.display.image(self.image)
 
